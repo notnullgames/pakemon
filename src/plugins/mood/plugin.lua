a62530e4b25d0b3408017d5dc7b54dc89ab74f57
David Konsumer <konsumer@jetboystudio.com>
2020-08-10 21:28:17 -0700

    fix sad animation and scale

--- src/plugins/mood/plugin.lua
+++ src/plugins/mood/plugin.lua
@@ -6,8 +6,8 @@ function MoodState:enter()
     image = love.graphics.newImage("assets/sprites1.png")
     local g = anim8.newGrid(32, 32, image:getWidth(), image:getHeight())
     animations = {
-        sad = anim8.newAnimation(g(7,5, 9,5), 0.5),
-        happy = anim8.newAnimation(g(4,5, 5,5, 6,5), 0.5)
+        sad = anim8.newAnimation(g(7,5, 8,5, 9,5), 0.5),
+        happy = anim8.newAnimation(g(4,5, 6,5), 0.5)
     }
 end
 
@@ -16,7 +16,11 @@ function MoodState:update(dt)
 end
 
 function MoodState:draw()
-    animations[self.mood]:draw(image, 145, 70)
+    love.graphics.push()
+    love.graphics.scale(2, 2)
+    animations[self.mood]:draw(image, 62, 25)
+    love.graphics.pop()
+    
     love.graphics.printf(self.mood, FontHeader, 0, 100, 320, "center")
 end
 
