d57cbc087b4195879d6c3dcbc6dea68b8d2e625d
David Konsumer <konsumer@jetboystudio.com>
2020-08-10 19:04:29 -0700

    center text and use header font

--- src/plugins/mood/plugin.lua
+++ src/plugins/mood/plugin.lua
@@ -2,7 +2,7 @@ local MoodState = { mood = "happy" }
 
 function MoodState:draw()
     -- TODO: put your code here that draws an animation based on self.mood
-    love.graphics.print(self.mood, 100, 100)
+    love.graphics.printf(self.mood, FontHeader, 0, 100, 320, "center")
 end
 
 return MoodState
