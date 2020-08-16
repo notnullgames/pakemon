cc278643e55bee35a4f9e66f5ac274b2a2766c86
David Konsumer <konsumer@jetboystudio.com>
2020-08-16 03:37:17 -0700

    added notnull state

--- src/plugins/welcome/state_welcome.lua
+++ src/plugins/welcome/state_welcome.lua
@@ -1,7 +1,13 @@
 local StateWelcome = {}
 
+local logo = love.graphics.newImage("plugins/welcome/notnullgames.png")
+local voice = love.audio.newSource("plugins/welcome/notnullgames.ogg", "stream")
+
+logo:setFilter('nearest', 'nearest')
+
 -- do your setup here
 function StateWelcome:enter(dt)
+  voice:play()
 end
 
 -- do your updates here
@@ -10,8 +16,10 @@ end
 
 -- do your draw calls here
 function StateWelcome:draw()
-  love.graphics.setFont(FontHeader)
-  love.graphics.print("PUT WELCOME MESAGE HERE", 65, 100)
+  love.graphics.push()
+  love.graphics.scale(4, 4)
+  love.graphics.draw( logo, 8.5, -2)
+  love.graphics.pop()
 end
 
 -- do your input-handling here
