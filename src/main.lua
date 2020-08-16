ba659e825b426663923269a330253f76b098d99a
ctol <tolstoyclout@gmail.com>
2020-08-16 15:19:32 -0700

    updated menu with into

--- src/main.lua
+++ src/main.lua
@@ -40,6 +40,7 @@ plugins.personality = require "plugins.personality.plugin"
 plugins.airplanes = require "plugins.airplanes.plugin"
 plugins.welcome = require "plugins.welcome.plugin"
 plugins.city_demo = require "plugins.city_demo.plugin"
+plugins.intro = require "plugins.intro.plugin"
 
 -- call current GameState's enter() on hot-reload
 lurker.postswap = function()
