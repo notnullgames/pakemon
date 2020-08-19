62a2d7e058486a725b2280e1eb1517fd74f0a352
ctol <tolstoyclout@gmail.com>
2020-08-18 21:03:31 -0700

    testing / not working

--- src/plugins/net_demo/state_net_demo.lua
+++ src/plugins/net_demo/state_net_demo.lua
@@ -10,74 +10,98 @@ NetworkImages = {
     phone = love.graphics.newImage("plugins/net_demo/phone.png")
 }
 
--- demo data - get this from nmap scan
-local network = {
-    { ip = "192.168.0.2", hostname = "gir.local", os="linux", type="computer" },
-    { ip = "192.168.0.3", hostname = "tak.local", os="apple", type="phone" },
-    { ip = "192.168.0.4", hostname = "gaz.local", os="windows", type="computer" },
-    { ip = "192.168.0.5", hostname = "dib.local", os="linux", type="computer" },
-    { ip = "192.168.0.6", hostname = "zim.local", os="apple", type="computer" },
-    { ip = "192.168.0.7", hostname = "taz.local", os="android", type="phone" }
-}
-local networkName='invaders' -- get this from wifi
+local network = httpGetJson("http://127.0.0.1:8081/api/session/lan")
 
--- current menu choice
-local selection = 1
+local function net_table()
+    for index,host in pairs(network) do 
+        print("index: " .. index)
+        for c,t in pairs(host) do
+            print(c,t)
+            for key,value in pairs(t) do
+                -- for key,table in pairs(k) do
+                --     print(key,table)
+                -- end
+                print(key,value)     
+                print("######################################")           
+            end 
+        end 
+    end
+end
 
-local camera = Camera(160, 120)
 
 function StateNetDemo:draw()
-    camera:attach()
-    for x,c in pairs(network) do
-        local y = 40 + (x-1) * 60
-        
-        -- draw text
-        love.graphics.printf(c.ip .. "\n" .. c.hostname, FontBasic, 130, y+10, 130, "right")
-        
-        -- draw device-type image
-        love.graphics.draw( NetworkImages[c.type], 100, y, 0, 0.6, 0.6)
-        
-        -- draw OS icon
-        if c.type == "computer" then
-            love.graphics.draw( NetworkImages[c.os], 116, y+8, 0, 0.4, 0.4)
-        else
-            love.graphics.draw( NetworkImages[c.os], 119, y+18, 0, 0.4, 0.4)
-        end
-        
-        -- draw border around current selection
-        if x == selection then
-            love.graphics.rectangle( "line", 80, y-10, 200, 60 )
-        end
-    end
-    camera:detach()
-    -- draw a rectangle to hide the other stuff
-    love.graphics.setColor(0,0,0,1)
-    love.graphics.rectangle( "fill", 0, 0, 320, 30 )
-    love.graphics.setColor(1,1,1,1)
 
-    love.graphics.printf(networkName, FontHeader, 10, 5, 300, "center")
+ 
+    --    love.graphics.print(#network, 20, 20, 0, 4, 4)
+    -- for _,v in ipairs(network) do
+    --     love.graphics.print(v)
+    -- end
 end
 
-function StateNetDemo:update(dt)
-    camera:lookAt(160, 120 + ((selection -1) * 60))
-end
+
+
+
+-- current menu choice
+--local selection = 1
+
+-- local camera = Camera(160, 120)
+-- function StateNetDemo:draw()
+--     camera:attach()
+--     for x,c in pairs(network) do
+
+--         local y = 40 + (x-1) * 60
+        
+--         -- draw text
+--         love.graphics.printf(c.ip .. "\n" .. c.hostname, FontBasic, 130, y+10, 130, "right")
+        
+--         -- draw device-type image
+--         love.graphics.draw( NetworkImages[c.type], 100, y, 0, 0.6, 0.6)
+        
+--         -- draw OS icon
+--         if c.type == "computer" then
+--             love.graphics.draw( NetworkImages[c.os], 116, y+8, 0, 0.4, 0.4)
+--         else
+--             love.graphics.draw( NetworkImages[c.os], 119, y+18, 0, 0.4, 0.4)
+--         end
+        
+--         -- draw border around current selection
+--         if x == selection then
+--             love.graphics.rectangle( "line", 80, y-10, 200, 60 )
+--         end
+--     end
+--     camera:detach()
+--     -- draw a rectangle to hide the other stuff
+--     love.graphics.setColor(0,0,0,1)
+--     love.graphics.rectangle( "fill", 0, 0, 320, 30 )
+--     love.graphics.setColor(1,1,1,1)
+
+--     love.graphics.printf(networkName, FontHeader, 10, 5, 300, "center")
+-- end
+
+-- function StateNetDemo:update(dt)
+--     camera:lookAt(160, 120 + ((selection -1) * 60))
+-- end
 
 function StateNetDemo:pressed(button)
-    if button == "up" then
-        selection = selection - 1
-        if selection < 1 then
-            selection = #network
-        end
-        SoundMove:stop()
-        SoundMove:play()
-    end
-    if button == "down" then
-        selection = selection + 1
-        if selection > #network then
-            selection = 1
-        end
-        SoundMove:stop()
-        SoundMove:play()
+    -- if button == "up" then
+    --     selection = selection - 1
+    --     if selection < 1 then
+    --         selection = #network
+    --     end
+    --     SoundMove:stop()
+    --     SoundMove:play()
+    -- end
+    -- if button == "down" then
+    --     selection = selection + 1
+    --     if selection > #network then
+    --         selection = 1
+    --     end
+    --     SoundMove:stop()
+    --     SoundMove:play()
+    -- end
+
+    if button == "y" then
+        net_table()
     end
     if button == "b" then
         SoundBack:play()
