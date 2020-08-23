f9bbc7d32d10ba8b5576b56a1e624ef97dab2bc3
David Konsumer <konsumer@jetboystudio.com>
2020-08-23 16:36:36 -0700

    move plane map to demos

--- src/plugins/airplanes/plugin.lua
+++ src/plugins/airplanes/plugin.lua
@@ -6,8 +6,8 @@ local PluginPlane = {}
 
 -- this gets called when the plugin is loaded
 function PluginPlane:load()
-    -- plugins.menu.main:addItem("Track Planes", function() Gamestate.switch(StatePlaneManager) end)
-    plugins.menu.main:addItem("Track Planes", function() Gamestate.switch(StatePlaneMap) end)
+    plugins.menu.main:addItem("Track Planes", function() Gamestate.switch(StatePlaneManager) end)
+    plugins.menu.main:addItem("Plane Map", function() Gamestate.switch(StatePlaneMap) end, 2)
     plugins.credits:add("Dynamic map images were provided by mapbox. Airplane image came from anim8's 1945 demo.")
     plugins.credits:add("dump1090 provides plane radio information.")
 end
